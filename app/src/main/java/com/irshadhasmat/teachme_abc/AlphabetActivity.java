3118f9803fefabb22bc3693088ca2ca6a64c6eb4
Mohammed Irshad <irshad_no1@live.com>
2017-04-07 00:21:07 +0530

    Added alphabets grid

---
+++ app/src/main/java/com/irshadhasmat/teachme_abc/AlphabetActivity.java
@@ -0,0 +1,47 @@
+package com.irshadhasmat.teachme_abc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class AlphabetActivity extends Activity {
+
+    GridView gridView;
+
+    static final String[] numbers = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_alphabet);
+
+        gridView = (GridView) findViewById(R.id.gridView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, numbers);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
