57ef7b64f65cdb0a320f857252a23e6e82834255
Mohammed Irshad <irshad_no1@live.com>
2017-04-06 23:55:05 +0530

    Drawing pad added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TeachMe-ABC-Android
-Teach Me ABC App which can help a kid to learn ABC by his own way... This app is just for fun and dedicated to my kid.
+Teach-Me ABC is a App which can help a kid to learn ABC by his own way... This app is just for fun and dedicated to my son.
---
+++ app/src/main/java/com/irshadhasmat/teachme_abc/DrawingPadView.java
@@ -0,0 +1,66 @@
+package com.irshadhasmat.teachme_abc;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Irshad on 06/04/17.
+ */
+
+public class DrawingPadView extends View {
+    // setup initial color
+    private final int paintColor = Color.BLACK;
+    // defines paint and canvas
+    private Paint drawPaint;
+    // stores next circle
+    private Path path = new Path();
+
+    public DrawingPadView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        // Setup paint with color and stroke styles
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(50);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        // Checks for the event that occurs
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX, pointY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+        // Force a view to draw again
+        postInvalidate();
+        return true;
+    }
+}
